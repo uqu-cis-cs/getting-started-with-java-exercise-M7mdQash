fa24fe782c628f4f9cb792758ab23d1884436a87
M7mdQash <113006736+M7mdQash@users.noreply.github.com>
2022-09-14 19:16:14 +0300

    Update App.java

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -13,7 +13,7 @@ package edu.uqu.cs;
  
 public class App
 {
-    private static void main( String[] args )
+    public static void main( String[] args )
     {
         System.out.println( "Hello Mohammed S443003805" );
     }
